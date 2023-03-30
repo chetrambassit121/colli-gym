3dc6615282dd3807c0457e7a81fa58e3264ba96d
Chetram Bassit <chetrambassit121@gmail.com>
2023-03-30 10:30:01 -0400

    allowed host in prod

--- power_building/settings/prod.py
+++ power_building/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["coliseum-gym.herokuapp.com"]
+ALLOWED_HOSTS = ["https://coliseum-gym.herokuapp.com"]
 
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY_ID = env("AWS_SECRET_ACCESS_KEY_ID")
