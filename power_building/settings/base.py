2d16a6cb9792a9dde6e04a3a8e4c97462ea17900
Chetram Bassit <chetrambassit121@gmail.com>
2023-03-31 09:38:19 -0400

    .

--- power_building/settings/base.py
+++ power_building/settings/base.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  
+    # "whitenoise.middleware.WhiteNoiseMiddleware",  
 ]
 
 ROOT_URLCONF = "power_building.urls"
