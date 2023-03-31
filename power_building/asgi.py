0bb04a790ebc874d62f0262fda35c0dbdbb79e1e
Chetram Bassit <chetrambassit121@gmail.com>
2023-03-31 11:15:17 -0400

    admin staticfiles

--- power_building/asgi.py
+++ power_building/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "power_building.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "power_building.settings")
 
 application = get_asgi_application()
