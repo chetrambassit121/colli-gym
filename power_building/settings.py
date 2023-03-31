a17a1cc4757e4cbb0b66dbd3a38c1cd37deb1598
Chetram Bassit <chetrambassit121@gmail.com>
2023-03-31 11:34:12 -0400

    aws staticfiles

--- power_building/settings.py
+++ power_building/settings.py
@@ -236,26 +236,26 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
-# '''media files'''
-# MEDIA_URL = "/media/"  
-# MEDIA_ROOT = os.path.join(
-#     BASE_DIR, "media"
-# )  
+'''media files'''
+MEDIA_URL = "/media/"  
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "media"
+)  
 
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/4.1/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# # STATIC_URL = "static/"
-# STATIC_URL = "/static/"  
-# STATICFILES_DIRS = [  
-#     os.path.join(
-#         BASE_DIR, "static"
-#     ),  
-# ]
-# STATIC_ROOT = os.path.join(
-#     BASE_DIR, "staticfiles"
-# )  
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  
+# STATIC_URL = "static/"
+STATIC_URL = "/static/"  
+STATICFILES_DIRS = [  
+    os.path.join(
+        BASE_DIR, "static"
+    ),  
+]
+STATIC_ROOT = os.path.join(
+    BASE_DIR, "staticfiles"
+)  
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  
 
 
 '''
